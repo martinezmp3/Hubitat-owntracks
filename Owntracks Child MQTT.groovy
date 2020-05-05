eb7765444662ecf80853424b562ce39a46be01b2
martinezmp3 <martinez.mp3@gmail.com>
2020-05-05 16:11:08 -0400

    Update Owntracks Child MQTT.groovy

--- Owntracks Child MQTT.groovy
+++ Owntracks Child MQTT.groovy
@@ -1,17 +1,32 @@
 metadata {
   definition (name: "Owntracks Child MQTT Traking", namespace: "jorge.martinez", author: "Jorge Martinez") {
-		capability "Initialize"
-		capability "Sensor"
-		capability "Battery"
-	    capability "Beacon"
-	    capability "EstimatedTimeOfArrival"
-	    capability "PresenceSensor"
-	    attribute "latitude", "float"
-	    attribute "longitude", "float"
-	    attribute "battery", "float"
-	    attribute "speed ", "integrer"
-    	attribute "lastUpdated", "String"
-//	    command "SetLogLat", ["float" , "float"]
+      capability "Initialize"
+      capability "Sensor"
+      capability "Battery"
+      capability "Beacon"
+      capability "EstimatedTimeOfArrival"
+      capability "PresenceSensor"
+      attribute "topic", "String"
+      attribute "latitude", "float"
+      attribute "longitude", "float"
+      attribute "battery", "float"
+      attribute "speed", "integrer"
+      attribute "lastUpdated", "Date"
+      attribute "DevicelastUpdated", "String"
+      attribute "inregions", "String"
+      attribute "configuartion", "String"
+      attribute "stepsToday", "number"
+      attribute "stepsWeek", "number"
+      attribute "stepsYear", "number"
+      attribute "stepsLiveTime", "number"
+      attribute "epoch", "long" 
+      attribute "communicado", "boolean"
+      //command "SetLogLat", ["float" , "float"]
+      //command "setBattery",["number","boolean"]
+      command "RequestLocation"
+      command "addWaysPoint", ["string", "number", "string", "string"]
+      command "GetConfiguration"
+      command "convertDateUnix", ["long"]
   }
 	preferences {
 	section("settings"){
@@ -19,14 +34,66 @@ metadata {
 		}
 		}
 	}
+def SetIncommunicado (){
+    state.communicado = false
+}
+String convertDateUnix (edate){
+    def newdate = new java.text.SimpleDateFormat("MM/dd/yyyy HH:mm:ss").format(new java.util.Date (edate*1000));
+    log.debug newdate
+    return newdate
+}
 
-def UpdateData (lat , lon, bat, spe) {
-	sendEvent(name: "latitude", value: lat, displayed: false)
-	sendEvent(name: "longitude", value: lon, displayed: false)
-	sendEvent(name: "battery", value: bat, displayed: false)
-	sendEvent(name: "speed", value: spe, displayed: false)
+def updateepoch (Nepoch){
+    state.epoch = Nepoch
+    log.debug convertDateUnix (Nepoch)
+    state.DevicelastUpdated = convertDateUnix (Nepoch)
+}
 
-	def nowDay = new Date().format("MMM dd", location.timeZone)
+
+def updateconfiguartion(confg){
+    log.debug confg
+    state.configuartion = conf
+}
+def GetConfiguration(){
+    parent.publishMsg ("${state.topic}/cmd",'{"_type":"cmd","action":"dump"}')
+}
+def RequestLocation (){
+    parent.publishMsg ("${state.topic}/cmd",'{"_type": "cmd","action": "reportLocation"}')
+}
+def Updatetopic (top){
+//    sendEvent(name: "topic", value: top)
+    state.topic = top
+}
+def Updatelatitude (lat){
+    sendEvent(name: "latitude", value: lat)
+    state.latitude = lat
+}
+def Updatelongitude (lon){
+    sendEvent(name: "longitude", value: lat)
+    state.latitude = lat
+}
+def Updatebattery (bat){
+    sendEvent(name: "battery", value: bat)
+}
+def Updatespeed (spe){
+    sendEvent(name: "speed", value: spe)
+}
+def Updateinregions (region){
+    sendEvent(name: "inregions", value: region)
+    state.inregions = region
+}
+    
+def lastUpdated(){
+    def nowDay = new Date().format("MMM dd", location.timeZone)
     def nowTime = new Date().format("h:mm a", location.timeZone)
+    state.lastUpdated = nowDay + " at " + nowTime
     sendEvent(name: "lastUpdated", value: nowDay + " at " + nowTime, displayed: false)
+}
+def Updatepresence (pre){sendEvent(name: "presence", value: pre)}
+def UpdateData (lat, lon) {
+    if (!communicado) state.communicado = true
+	sendEvent(name: "latitude", value: lat)
+    state.latitude = lat
+	sendEvent(name: "longitude", value: lon)
+    state.longitude = lon
 }
